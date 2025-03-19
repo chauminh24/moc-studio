6b36f1d0b53a38439d35f00d920f10f1967e4d67
Minh Chau <g00425239@atu.ie>
2025-03-19 13:53:05 +0000

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,19 +3,19 @@ const Home = () => {
     <div className="@container main homepage">
       <div className="h-[100vh] relative">
         <img
-          src="/placeholder/5x4_holder.png"
+          src="/5x4_holder.png"
           alt="Header Image"
           className="h-full absolute object-cover right-0"
         />
       </div>
       <div className="md:px-25 px-10 pt-10">
         <img
-          src="/placeholder/square_holder.png"
+          src="square_holder.png"
           alt="Header Image"
           className="h-auto md:w-[40vw] w-full"
         />
         <img
-          src="/placeholder/square_holder.png"
+          src="square_holder.png"
           alt="Header Image"
           className="h-auto md:w-[40vw] float-right w-full"
         />
