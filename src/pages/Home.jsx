1100f22b5940d3dc73504360cdec6e515eb8e37c
Minh Chau <g00425239@atu.ie>
2025-03-19 13:56:49 +0000

    Update image paths to use placeholder directory in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,19 +3,19 @@ const Home = () => {
     <div className="@container main homepage">
       <div className="h-[100vh] relative">
         <img
-          src="/5x4_holder.png"
+          src="/placeholder/5x4_holder.png"
           alt="Header Image"
           className="h-full absolute object-cover right-0"
         />
       </div>
       <div className="md:px-25 px-10 pt-10">
         <img
-          src="square_holder.png"
+          src="/placeholder/square_holder.png"
           alt="Header Image"
           className="h-auto md:w-[40vw] w-full"
         />
         <img
-          src="square_holder.png"
+          src="/placeholder/square_holder.png"
           alt="Header Image"
           className="h-auto md:w-[40vw] float-right w-full"
         />
