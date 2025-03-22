5245ae878729361fb88df75d17f34ae1aa09df1d
Minh Chau <g00425239@atu.ie>
2025-03-22 22:14:43 +0000

    Fix import path for ProductCard in ShopByRoom component

--- src/pages/ShopByRoom.jsx
+++ src/pages/ShopByRoom.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
-import ProductCard from "../components/ProductCard";
+import ProductCard from "./ProductCard";
 
 const ShopByRoom = () => {
   const { category } = useParams(); // Get the category from the URL
