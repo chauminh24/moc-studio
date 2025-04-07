29daa458ef452bf400ff99b2685453a8b7d15fa3
Minh Chau <g00425239@atu.ie>
2025-04-07 19:35:45 +0100

    Adjust button positioning in ProductCard component for improved layout

--- src/pages/ProductCard.jsx
+++ src/pages/ProductCard.jsx
@@ -23,7 +23,7 @@ const ProductCard = ({ product }) => {
       </div>
   
       {/* Add to Cart Button */}
-      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2">
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
         <button
           onClick={handleAddToCart}
           className={`p-3 transition-all duration-300 
