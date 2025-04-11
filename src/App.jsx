2fa06033ab11e6827974e130a12ebda261dd62f4
Minh Chau <g00425239@atu.ie>
2025-04-11 09:23:52 +0100

    Add ProductDetailsPage import and ensure product details route is properly defined

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import SearchResults from "./pages/SearchResults";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
 import NotFound from "./pages/NotFound";
 
 const App = () => {
@@ -74,7 +75,7 @@ const App = () => {
               <Route path="/login" element={<LoginPage />} />
               <Route path="/register" element={<RegisterPage />} />
               <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-              
+
               {/* Product Details Route */}
               <Route path="/product/:productId" element={<ProductDetailsPage />} />
 
